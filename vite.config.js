b1b990cbab109c8b77a83b9fdebea09ef1421bee
schokett <skrollanelenazimmermann@gmail.com>
2026-07-01 11:55:18 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  base: "/portfolioClean/",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        impressum: resolve(__dirname, "impressum.html"),
+      },
+    },
+  },
 });
